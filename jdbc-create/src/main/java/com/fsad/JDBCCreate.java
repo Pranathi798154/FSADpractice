ecc52b5d7e9c698aeef222e2e7aacf0e3ca50a9c
NOMULA PRANATHI <2400032306@kluniversity.in>
2026-01-01 22:06:53 +0530

    jdbc
    jdbc

---
+++ jdbc-create/src/main/java/com/fsad/JDBCCreate.java
@@ -0,0 +1,24 @@
+package com.fsad;
+import java.sql.*;
+
+public class JDBCCreate {
+
+	public static void main(String[] args) {
+		String url="jdbc:mysql://localhost:3306/fsad";
+		String usr="root";
+		String pwd="Pranathi15@2006";
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,usr,pwd);
+				System.out.println("Connection Established");
+				String query="create table if not exists Student(id int primary key auto_increment, "+"name varchar(20)"+")";
+				Statement st=con.createStatement();
+				st.execute(query);
+				con.close();
+				}
+                catch(Exception e)  
+		{
+                	e.printStackTrace();
+		}
+}
+}
